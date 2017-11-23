e85e0c1d6e26aada3952d6880fd2334366d54874
이예린\Yerin <ye_reen@naver.com>
2017-11-24 03:24:53 +0900

    1124

---
+++ week12 - 복사본/routes/gett.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var Book = require('../models/book');
+
+
+router.get('/', function(req,res, next){
+Book.find(function(err, books){
+if(err) return res.status(500).send({error: 'database failure'});
+res.render('get', {items:Book});
+})
+});
+module.exports = router;
---
+++ week12 - 복사본/views/index.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+ <meta charset="UTF-8">
+</head>
+<body>
+ <h1>파일을 선택하세요</h1>
+ <form  action='/upload' method="post" enctype="multipart/form-data" > 
+  <font size="5">파일 선택</font>&nbsp;&nbsp;<input type="text" name="name" />
+        <br>
+        <br>
+        <input type='file' name='upload' /><br>
+        <br>
+        <input type='submit' />
+ </form>
+</body>
+</html>
