9c4d2331fb5f6c7cbc023b403ae2375cdf14ed29
이예린\Yerin <ye_reen@naver.com>
2017-12-15 22:35:17 +0900

    웹 & 앱 LAB 최종

---
+++ week15최종/myapp/models/book.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema({
+	photo:String,
+	name:String,
+	address:String,
+	phone:String,
+	comments:String,
+	score:String,
+	reply:[{
+	  person:String,
+	memo:String
+	}],
+	reservation:[{
+	dbman:String,
+	dbmany:String,
+	dbwhen:String,
+	dbtime:String
+	}]
+});
+
+module.exports = mongoose.model('book',bookSchema);
